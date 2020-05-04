ce7710b9de3881e620b305771e9f2d10f832d3bb
Sam <samlconsidine@gmail.com>
2020-05-04 08:45:46 -0400

    Clicks are being logged and pushed into an array

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ElfCard from "./components/ElfCard";
 import images from "./images.json";
 
-// function App() {
-//   return (
-//       <h1>Clicky Game</h1>
-      
-//   );
-// }
-
-class App extends React.Component {
-  state= {
-    images
+class App extends Component {
+  state = {
+    images,
+    score: 0,
+    chosen: [],
+    newPick: ""
   };
+
+  chosenElf = id => {
+    const newPick = this.state.images.filter(image => image.id === id);
+    this.setState({ newPick: newPick });
+    console.log(this.state.newPick);
+    this.setState(state => {
+      const chosen = state.chosen.concat(state.newPick);
+      return {
+        chosen,
+        newPick: ""
+      }
+    });
+
+    console.log(this.state.chosen);
+  };
+
+  // checkGame = (newPick) => {
+  //   if (this.state.chosen.includes(newPick)) {
+  //     console.log("character has already been added");
+  //   } else {
+  //     console.log("added new character");
+  //   };
+  // }
+  
+  // chosenElf = id => {
+  //   const newPick = this.state.images.filter(picked => picked.id === id);
+  //   this.setState({ chosen: [newPick] });
+  //   this.checkGame(newPick);
+  // }
+  
+  handleIncrement = () => {
+    this.setState({ score: this.state.score + 1 });
+    console.log(this.state.score);
+  }
 
   render() {
     return (
@@ -21,15 +51,17 @@ class App extends React.Component {
           <div className="row">
               {this.state.images.map(image => (
                 <ElfCard
-                  className="col-xs-2 col-sm-3"
-                  id={image.id}
-                  key={image.id}
-                  name={image.name}
-                  image={image.image}
+                className="col-xs-2 col-sm-3"
+                chosenElf={this.chosenElf}
+                id={image.id}
+                key={image.id}
+                name={image.name}
+                image={image.image}
                 />
               ))}
           </div>
         </div>
+        <h1 onClick={this.handleIncrement}>click</h1>
       </div>
     );
   }
--- clickygame/src/components/ElfCard/index.js
+++ clickygame/src/components/ElfCard/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./style.css"
 
+
 function ElfCard(props) {
     return (
         <div className="card">
-            <div className="img-container">
-                <img alt={props.name} src={props.image} />
+            <div className="img-container text-align-center">
+                <img alt={props.name} src={props.image}/>
             </div>
-        </div>
-      
+            <span onClick={() => props.chosenElf(props.id)} className="remove">
+                x
+            </span>
+        </div>      
     );
 }
 
