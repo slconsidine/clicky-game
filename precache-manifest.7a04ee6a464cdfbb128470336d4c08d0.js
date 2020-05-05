4fd92d3e91602f529e30161aba1bf984f1d13b20
Sam <samlconsidine@gmail.com>
2020-05-05 18:29:43 -0400

    Updates

---
+++ precache-manifest.7a04ee6a464cdfbb128470336d4c08d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8739aadd86db8904e3a6d41951d0fed2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "688688c8367627e19a11",
+    "url": "/clicky-game/static/css/main.d843ffde.chunk.css"
+  },
+  {
+    "revision": "65f3a8192788d3fdad75",
+    "url": "/clicky-game/static/js/2.78097946.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.78097946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688688c8367627e19a11",
+    "url": "/clicky-game/static/js/main.d8ed8213.chunk.js"
+  },
+  {
+    "revision": "b1a02565af27a65f66a4",
+    "url": "/clicky-game/static/js/runtime-main.552a697a.js"
+  }
+]);
